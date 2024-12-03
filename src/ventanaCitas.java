20250272bf6dedbd81cc79f0b59b34adba24f0a9
Fernanda Morales <al03039029@tecmilenio.com>
2024-12-02 22:46:18 -0600

    ventanaCitas.java y .form

---
+++ src/ventanaCitas.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ventanaCitas {
+    private JTextField txtIDCita;
+    private JTextField txtFechaCita;
+    private JTextField txtMotivoCita;
+    private JTextField txtDoctorCita;
+    private JTextField txtPacienteCita;
+    private JButton btnAgregar;
+    private JButton btnBuscar;
+}
